adc4e2e9d9e69ad31e547e9c04d5fad7e9d5cdda
Mustafa Salih Oğuz <mustafasalihoguz@gmail.com>
2020-01-31 07:29:56 +0300

    Add speedup function

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject SpeedupButton;
+
+    public void Speedup()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 5;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
